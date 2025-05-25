41e78d5680f75e8be7c1ce4ab40b8cd7520915e0
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-05-25 12:24:31 -0500

    Use input().lower() to simplify.py

--- car_manual/car_manual.py
+++ car_manual/car_manual.py
@@ -1,16 +1,16 @@
 command=""
 while True:
-    command=input(">")
-    if command.lower()=="help":
+    command=input(">").lower()
+    if command=="help":
         print('''
 start - to start the car
 stop - to stop the car
 quit - to quit the car''')
-    elif command.lower()=="start":
+    elif command=="start":
         print("Car has started.")
-    elif command.lower()=="stop":
+    elif command=="stop":
         print("Car has stopped.")
-    elif command.lower()=="quit":
+    elif command=="quit":
         print("Car quitted.")
     else:
         print("I don't understand.")
