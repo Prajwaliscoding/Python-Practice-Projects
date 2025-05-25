611f7c2fa7771b711328424e59cd212daea5696f
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-05-25 12:47:48 -0500

    If started initially say already started.py

--- car_manual/car_manual.py
+++ car_manual/car_manual.py
@@ -1,4 +1,5 @@
 command=""
+started=False
 while True:
     command=input(">").lower()
     if command=="help":
@@ -7,9 +8,17 @@ start - to start the car
 stop - to stop the car
 quit - to quit the car''')
     elif command=="start":
-        print("Car has started.")
+        if started:
+            print("Car has already started.")
+        else:
+            started=True
+            print("Car has started.")
     elif command=="stop":
-        print("Car has stopped.")
+        if not started:
+            print("Car has already stopped.")
+        else:
+            started=False
+            print("Car has stopped.")
     elif command=="quit":
         print("Car quitted.")
     else:
