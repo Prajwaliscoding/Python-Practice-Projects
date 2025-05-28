75afe6693f81aa76280494311cfc375306db8d4d
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2025-05-28 12:26:15 -0500

    Create Digits_to_Words.py

---
+++ Digits_to_Words/Digits_to_Words.py
@@ -0,0 +1,16 @@
+digits=input("Enter the digits:\n")
+words={
+    "1":"one",
+    "2":"two",
+    "3":"three",
+    "4":"four",
+    "5":"five",
+    "6":"six",
+    "7":"seven",
+    "8":"eight",
+    "9":"nine",
+}
+output=""
+for i in digits:
+    output=output+words.get(i)+" "
+print(output)
